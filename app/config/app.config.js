44f5c5fb1d6b0f21524a2362b4634bae09ca9d98
Shubham Sharma <shan61916@gmail.com>
2021-01-12 22:16:08 +0530

    Add app configs

---
+++ app/config/app.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	YT: {
+	SEARCH_URL: 'https://youtube.googleapis.com/youtube/v3/search',
+	KEYS: [
+	],
+	QUERY_ELEMENT: 'Cricket'
+ },
+	cacheKey: 'YT/keyIndex',
+	pageTokenKey: 'YT/pageToken',
+	fetchLimit: 20
+};
