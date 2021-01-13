f28264cbbfff7b465a1914f0e9bfd296c62c3599
Shubham Sharma <shan61916@gmail.com>
2021-01-14 04:50:31 +0530

    Update jsDoc for uncovered functions

--- app/controllers/video-update-job.js
+++ app/controllers/video-update-job.js
@@ -4,6 +4,10 @@ const request = require("request");
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache();
 
+/**
+ * @description gets the current active access key for YT API
+ * @returns {String} the active key
+ */
 async function getCurrentKey() {
 	const keys = config.YT.KEYS;
 	const cacheKey = config.cacheKey;
@@ -15,6 +19,10 @@ async function getCurrentKey() {
 	return keys[keyIndex];
 }
 
+
+/**
+ * @description changes the YT key to the one least recently used
+ */
 function incrementKeyIndex() {
 	const totalKeys = config.YT.KEYS.length;
 	const cacheKey = config.cacheKey;
@@ -26,6 +34,14 @@ function incrementKeyIndex() {
 	myCache.set(cacheKey, (keyIndex + 1)%totalKeys);
 }
 
+
+/**
+ * @description fetches the list of videos from YT for a given page
+ * uses the first page if page-token is not present
+ * @param {String} accessKey the accessKey for YT API
+ * @param {String} [pageToken=null] the page token to be fetched, if any
+ * @returns {Object} the list of videos with their snippet data
+ */
 function getVideoResults(accessKey, pageToken = null) {
 	return new Promise((resolve, reject) => {
 		const defaultThreshHold = new Date();
@@ -61,6 +77,13 @@ function getVideoResults(accessKey, pageToken = null) {
 	});
 }
 
+
+/**
+ * @description parses the videos and converts them to the DB model format
+ *
+ * @param {Object} videoObject list of videos returned by YT API
+ * @returns {Object} DB model compliant video object
+ */
 function parseVideos(videoObject) {
 	const videoItems = videoObject.items;
 	const parsedVideos = videoItems.map((item) => {
@@ -80,6 +103,13 @@ function parseVideos(videoObject) {
 	return parsedVideos
 }
 
+
+/**
+ * @description inserts videos in the DB in batch
+ *
+ * @param {Array<Object>} videos array of video objects
+ * @return {boolean} true, if insertion is successful
+ */
 async function bulkInsertVideos(videos) {
 	if (!Array.isArray(videos) && !videos.length) {
 		return true;
@@ -93,17 +123,36 @@ async function bulkInsertVideos(videos) {
 	}
 }
 
+
+/**
+ * @description fetches the pageToken to be fetched next
+ *
+ * @return {String} the token of the page to be fetched
+ */
 async function getNextPageToken() {
 	const pageTokenKey = config.pageTokenKey;
 	let token = await myCache.get(pageTokenKey);
 	return token || null;
 }
 
+
+/**
+ * @description updates the next page token in cache
+ *
+ * @param {String} token the token to be set in cache
+ * @return {Promise} containing response of cache update
+ */
 async function updateNextPageToken(token) {
 	const pageTokenKey = config.pageTokenKey;
 	return myCache.set(pageTokenKey, token);
 }
 
+
+/**
+ * @description function that calls the YT API and fetches videos in DB
+ *
+ * @return {Number} the total number of records updated
+ */
 async function updateVideoFeed() {
 	// gets least recently used key
 	const [ key, pageToken ]  = await Promise.all([ getCurrentKey(), getNextPageToken() ]);
